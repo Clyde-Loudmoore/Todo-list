a758c34cabe82d85992f5eb5ad68309ccac91371
Denis Stanovoy <stanovoyd@yandex.ru>
2022-11-15 11:31:25 +0300

    implemented a counter of active and completed tasks

--- src/App/App.js
+++ src/App/App.js
@@ -10,28 +10,15 @@ const App = () => {
   // const [todos, setTodos] = React.useState(
   //   JSON.parse(localStorage.getItem("todos")) || []
   // );
-  // const [filteredTodos, setFilteredTodos] = React.useState("all");
 
   // React.useEffect(() => {
   //   localStorage.setItem("todos", JSON.stringify(todos));
   // }, [todos]);
 
-  // const todoFilter = React.useMemo(() => {
-  //   if (filteredTodos === "complited") {
-  //     return todos.filter((item) => item.status === true);
-  //   }
-  //   if (filteredTodos === "active") {
-  //     return todos.filter((item) => item.status === false);
-  //   }
-  //   return todos;
-  // }, [filteredTodos, todos]);
-
   return (
     <TodoContainer>
       <Header />
-      <Main
-      // onClick={setFilteredTodos}
-      />
+      <Main />
       <Footer />
     </TodoContainer>
   );
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,33 +1,35 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { filteringTask } from "../../store/todoSlice";
 
 import FooterContainer from "./FooterContainer.styled";
 
-// const countComplitedTask = () => {
-//   let count = 0;
-//   todos.map((todo) => {
-//     if (todo.status) {
-//       return count++;
-//     } else if (!todo.status) {
-//       return count;
-//     }
-//   });
-//   return count;
-// };
+const Footer = () => {
+  const newTodos = useSelector(filteringTask);
 
-const Footer = (props) => {
-  // const counterTask = React.useMemo(
-  //   () => countComplitedTask(props.todos),
-  //   [props.todos]
-  // );
+  const countComplitedTask = () => {
+    let count = 0;
+    newTodos.map((todo) => {
+      if (todo.status) {
+        return count++;
+      } else if (!todo.status) {
+        return count;
+      }
+      return count;
+    });
+    return count;
+  };
+
+  const counterTask = useSelector(() => countComplitedTask(newTodos));
 
   return (
     <FooterContainer>
-      {/* {props.todos.length > 0 && (
+      {newTodos.length > 0 && (
         <div className="todo__content-wrapper">
-          <p>total active: {props.todos.length - counterTask}</p>
+          <p>total active: {newTodos.length - counterTask}</p>
           <p>total complited: {counterTask}</p>
         </div>
-      )} */}
+      )}
     </FooterContainer>
   );
 };
